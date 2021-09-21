73d5b88968dfc27ebc3762000eb2b28e6369df27
Thomas Pham <46455958+PhamilyExpress@users.noreply.github.com>
2021-09-20 19:47:12 -0600

    fixed part 6

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import  cgi, secret
+import  cgi, os, requests, secret
 from templates import login_page, secret_page
 
 form = cgi.FieldStorage()
@@ -9,12 +9,16 @@ password = form.getvalue('password')
 
 print("Content-Type: text/html;")
 if username == secret.username and password == secret.password:
-  print("Set-Cookie:UserID = %s;" % username)
+  print("Set-Cookie:Username = %s;" % username)
   print("Set-Cookie:Password = %s;" % password)
+
+request = requests.get('http://localhost:8080/login.py')
+usernameCookie = request.cookies('Username')
+passwordCookie = request.cookies('Password')
 
 if not username and not password:
   print(login_page())
-elif username == secret.username and password == secret.password:
+elif usernameCookie == secret.username and passwordCookie == secret.password:
   print(secret_page(username, password))
 else:
   print(login_page())
